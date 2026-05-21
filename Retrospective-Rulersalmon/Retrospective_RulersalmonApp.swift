427ee9c29fdf52881d41085ce02452df8e73a9bb
DevPaul <ryu990305@icloud.com>
2026-05-21 14:48:47 +0900

    Initial Commit

---
+++ Retrospective-Rulersalmon/Retrospective_RulersalmonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Retrospective_RulersalmonApp.swift
+//  Retrospective-Rulersalmon
+//
+//  Created by DevPaul on 5/21/26.
+//
+
+import SwiftUI
+
+@main
+struct Retrospective_RulersalmonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
