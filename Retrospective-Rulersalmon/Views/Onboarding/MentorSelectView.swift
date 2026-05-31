f939c4d99fe925b5415b39d2ea7f0d1c248893a3
dlsundn <sw020406@naver.com>
2026-06-01 07:30:42 +0900

    feat : MentorSelectView 생성

---
+++ Retrospective-Rulersalmon/Views/Onboarding/MentorSelectView.swift
@@ -0,0 +1,18 @@
+//
+//  MentorSelectView.swift
+//  Retrospective-Rulersalmon
+//
+//  Created by dlsundn on 5/31/26.
+//
+
+import SwiftUI
+
+struct MentorSelectView: View {
+    var body: some View {
+      OnboardingTitle(headline: "나의 회고를 도와줄\n멘토를 선택해 주세요", subtitle: "멘토는 나중에 제한 없이 변경할 수 있어요.")
+    }
+}
+
+#Preview {
+    MentorSelectView()
+}
